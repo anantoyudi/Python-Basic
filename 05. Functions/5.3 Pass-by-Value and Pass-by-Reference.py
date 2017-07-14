064e9d62d2cde03b61b845c453509d2a9fb85587
Ananto Yudi <nantoyudi@gmail.com>
2017-07-14 08:07:21 +0700

    add codes result analysis

--- 05. Functions/5.3 Pass-by-Value and Pass-by-Reference.py
+++ 05. Functions/5.3 Pass-by-Value and Pass-by-Reference.py
@@ -1,11 +1,14 @@
 
-# Dalam pemrograman, parameter dapat di lewatkan kedalam suatu fungsi menggunakan dua cara, yaitu:
-# 1. Pass-by-Value (melewatkan parameter berdasarkan nilai)
-# 2. Pass-by-Reference (melewatkan berdasarkan alamat atau referensinya)
+"""
+Dalam pemrograman, parameter dapat di lewatkan kedalam suatu fungsi menggunakan dua cara, yaitu:
+1. Pass-by-Value (melewatkan parameter berdasarkan nilai)
+2. Pass-by-Reference (melewatkan berdasarkan alamat atau referensinya)
 
 
-# Python tidak mengenal Pass-by-Value. Ini berarti bahwa semua parameter fungsi dalam Python selalu di lewatkan
-# menggunakan Pass-by-Reference.
+Python tidak mengenal Pass-by-Value. Ini berarti bahwa semua parameter fungsi dalam Python selalu di lewatkan
+menggunakan Pass-by-Reference.
+"""
+
 
 
 # contoh pengiriman parameter di dalam Python.
@@ -17,6 +20,7 @@ def ubahNilai(p):
     return
 
 def main():
+    print("Output:")
     a = 5
 
     print("Sebelum pemanggilan fungsi")
@@ -43,3 +47,15 @@ p = 500
 Setelah pemanggilan fungsi
 a = 5
 """
+
+"""
+Hasil yang kita inginkan adalah a berubah menjadi 500 setelah fungsi ubahNilai() dipanggil. Untuk menganalisis
+kasus ini, kita perlu mengingat kembali bahwa bilangan menrupakan objek yang tidak dapat diubah (immutable).
+Pada saat program mengeksekusi statement ubahNilai(a), referensi p akan menunjuk ke objek yang sendang di tunjuk
+oleh referensi a, sehingga p akan bernilai 5. Alamat memory dari p juga akan sama dengan a. Selanjutnya, ketika 
+perintah p *= 100 di eksekusi, p akan menunjuk ke object baruyang nilainya 500 (berasal dari p * 100). Ini berarti
+bahwa saat ini a dan p masing-masing menunjuk ke objek yang berlainan, dengan alamat memory yang berbeda. a masih 
+menunjuk ke objek yang bernilai 5, sedangkan p sudah menunjuk ke object yang bernilai 500. Saat nilai a di panggil,
+maka yang akan tampil adalah 5, bukan 500.
+"""
+
