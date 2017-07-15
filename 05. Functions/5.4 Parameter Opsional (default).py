24933d60d2cd30accceeb1dd3d395600017e0723
Ananto Yudi <nantoyudi@gmail.com>
2017-07-15 21:03:54 +0700

    add infoSiswa() function

--- 05. Functions/5.4 Parameter Opsional (default).py
+++ 05. Functions/5.4 Parameter Opsional (default).py
@@ -7,6 +7,7 @@ def infoSiswa(nama, usia=30):
     print("Output:")
     print("name: {}" .format(nama))
     print("Usia: {}" .format(usia))
+    print("\n")
 
 infoSiswa("ananto")
 
@@ -18,5 +19,15 @@ Usia: 30
 
 """
 Pada contoh di atas, kita mendefinisikan parameter usia sebagai parameter default dengan nilai awal 30. ketika
-fungsi infoSiswa() di panggil dengan cara di atas. Maka fungsi akan menghasilkan nama ananto dan umur 30.
+fungsi infoSiswa() di panggil dengan cara di atas. Maka fungsi akan menghasilkan nama ananto dan umur 30. Jika 
+kita mendefinisikan fungsi diatas dengan dengan nilai berbeda untuk parameter usia maka hasil dari fungsi akan 
+seoerti di bawah ini.
+"""
+
+infoSiswa("yudi", usia=31)
+
+"""
+Output:
+name: yudi
+Usia: 31
 """
