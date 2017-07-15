267b487c76f733f52f74ee64e5c66a27d26df58f
Ananto Yudi <nantoyudi@gmail.com>
2017-07-15 20:58:14 +0700

    add infoSiswa() function

--- 05. Functions/5.4 Parameter Opsional (default).py
+++ 05. Functions/5.4 Parameter Opsional (default).py
@@ -1,3 +1,22 @@
 """
-Python juga mengizinkan kita untuk mendefinisikan parameter opsional (default) di dalam fungsi.
+Python juga mengizinkan kita untuk mendefinisikan parameter opsional (default) di dalam fungsi. Parameter default
+adalah parameter yang nilainya diinisialisasi pada saat fungsi di definisikan.
+"""
+
+def infoSiswa(nama, usia=30):
+    print("Output:")
+    print("name: {}" .format(nama))
+    print("Usia: {}" .format(usia))
+
+infoSiswa("ananto")
+
+"""
+Output:
+name: ananto
+Usia: 30
+"""
+
+"""
+Pada contoh di atas, kita mendefinisikan parameter usia sebagai parameter default dengan nilai awal 30. ketika
+fungsi infoSiswa() di panggil dengan cara di atas. Maka fungsi akan menghasilkan nama ananto dan umur 30.
 """
